73dee16d7a4583ba1144d6f7eb7f4d4a869ac503
SlavaDementev <kursant344@gmail.com>
2021-09-23 15:22:32 +0300

    change if to when

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -1,8 +1,8 @@
 package ru.netology
 
 fun main() {
-    val totalPrice = 1000_00
-    val musicLover = false
+    val totalPrice = 1001_00
+    val musicLover = true
 
     val sum = if (musicLover) {
         when (totalPrice) {
@@ -10,27 +10,15 @@ fun main() {
                 totalPrice - totalPrice * 1 / 100
             }
             in 1001_00..10000_00 -> {
-                totalPrice - 100_00
+                val tmp = totalPrice - 100_00
+                tmp - tmp * 1 / 100
             }
             else -> {
-                totalPrice - totalPrice * 5 / 100
+                val tmp = totalPrice - totalPrice * 5 / 100
+                tmp - tmp * 1 / 100
             }
         }
-    }
-    /* {
-        val i = if (totalPrice in 1..1000_00) {
-             totalPrice - totalPrice * 1 / 100
-
-         } else if (totalPrice in 1001_00..10000_00) {
-             val tmp = totalPrice - 100_00
-             tmp - tmp * 1 / 100
-         } else {
-             val tmp = totalPrice - totalPrice * 5 / 100
-             tmp - tmp * 1 / 100
-         }
-         i
-
-    }*/ else {
+    } else {
         when (totalPrice) {
             in 1..1000_00 -> {
                 totalPrice - totalPrice * 1 / 100
@@ -43,6 +31,7 @@ fun main() {
             }
         }
     }
-    val result = sum / 100
-    print("Цена с учетом скидки $result руб.")
+    val resultRub = sum / 100
+    val resultKop = sum % 100
+    print("Цена с учетом скидки $resultRub руб. $resultKop коп.")
 }
