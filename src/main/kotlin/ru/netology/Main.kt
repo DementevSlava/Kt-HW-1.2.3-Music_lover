aee6754e292d6b0712c9bcde3ca893c9917ed0fc
SlavaDementev <kursant344@gmail.com>
2021-09-23 17:42:45 +0300

    Not HardCod_2

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -6,31 +6,33 @@ fun main() {
     val condition1 = 1..1000
     val condition2 = 1001_00..10000_00
     val fixedDiscount = 100
+    val onePercent = 1 / 100
+    val fivePercent = 5 / 1000
 
     val sum = if (musicLover) {
         when (totalPrice) {
             in condition1 -> {
-                totalPrice - totalPrice * 1 / 100
+                totalPrice - totalPrice * onePercent
             }
             in condition2 -> {
                 val tmp = totalPrice - fixedDiscount
-                tmp - tmp * 1 / 100
+                tmp - tmp * onePercent
             }
             else -> {
-                val tmp = totalPrice - totalPrice * 5 / 100
-                tmp - tmp * 1 / 100
+                val tmp = totalPrice - totalPrice * fivePercent
+                tmp - tmp * onePercent
             }
         }
     } else {
         when (totalPrice) {
             in condition1 -> {
-                totalPrice - totalPrice * 1 / 100
+                totalPrice - totalPrice * onePercent
             }
             in condition2 -> {
                 totalPrice - fixedDiscount
             }
             else -> {
-                totalPrice - totalPrice * 5 / 100
+                totalPrice - totalPrice * fivePercent
             }
         }
     }
